b89b6996271126a931f8dfe6cf7fc6ae4c22d54d
evgxnx <birki192@gmail.com>
2023-06-23 21:05:07 +0600

    123

--- button-animation.js
+++ button-animation.js
@@ -8,3 +8,47 @@ $(document).ready(function() {
       }, 1000);
     });
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // Get all filter buttons
+    var filterButtons = document.querySelectorAll('.filter-button');
+  
+    // Add click event listener to each filter button
+    filterButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        var selectedCategory = this.getAttribute('data-category');
+  
+        // Show all menu items if "All" is selected
+        if (selectedCategory === 'all') {
+          showAllMenuItems();
+        } else {
+          // Hide all menu items
+          hideAllMenuItems();
+  
+          // Show menu items with selected category
+          var menuItems = document.querySelectorAll('.menu-item');
+          menuItems.forEach(function(item) {
+            var itemCategory = item.getAttribute('data-category');
+            if (itemCategory === selectedCategory) {
+              item.style.display = 'block';
+            }
+          });
+        }
+      });
+    });
+  });
+  
+  function showAllMenuItems() {
+    var menuItems = document.querySelectorAll('.menu-item');
+    menuItems.forEach(function(item) {
+      item.style.display = 'block';
+    });
+  }
+  
+  function hideAllMenuItems() {
+    var menuItems = document.querySelectorAll('.menu-item');
+    menuItems.forEach(function(item) {
+      item.style.display = 'none';
+    });
+  }
+  
--- comments.js
+++ comments.js
@@ -1,68 +1,67 @@
 const nameInput = document.getElementById('name-input');
-const ratingInputs = document.querySelectorAll('input[name="rating"]');
-const commentInput = document.getElementById('comment-input');
-const submitButton = document.getElementById('submit-button');
-const commentsContainer = document.getElementById('comments-container');
+  const ratingInputs = document.querySelectorAll('input[name="rating"]');
+  const commentInput = document.getElementById('comment-input');
+  const submitButton = document.getElementById('submit-button');
+  const commentsContainer = document.getElementById('comments-container');
 
-function submitComment() {
-  const name = nameInput.value;
-  let rating;
+  function submitComment() {
+    const name = nameInput.value;
+    let rating;
 
-  for (const input of ratingInputs) {
-    if (input.checked) {
-      rating = input.value;
-      break;
+    for (const input of ratingInputs) {
+      if (input.checked) {
+        rating = input.value;
+        break;
+      }
+    }
+
+    const comment = commentInput.value;
+
+    const commentElement = document.createElement('div');
+    commentElement.classList.add('comment');
+    commentElement.innerHTML = `
+      <p><strong>Name:</strong> ${name}</p>
+      <p><strong>Rating:</strong> ${rating}</p>
+      <p><strong>Comment:</strong> ${comment}</p>
+    `;
+
+    commentsContainer.appendChild(commentElement);
+
+    saveComment({ name, rating, comment });
+
+    nameInput.value = '';
+    commentInput.value = '';
+    ratingInputs[0].checked = true;
+  }
+
+  function saveComment(comment) {
+    let comments = localStorage.getItem('comments');
+    if (!comments) {
+      comments = [comment];
+    } else {
+      comments = JSON.parse(comments);
+      comments.push(comment);
+    }
+
+    localStorage.setItem('comments', JSON.stringify(comments));
+  }
+
+  function loadComments() {
+    const comments = localStorage.getItem('comments');
+
+    if (comments) {
+      JSON.parse(comments).forEach(({ name, rating, comment }) => {
+        const commentElement = document.createElement('div');
+        commentElement.classList.add('comment');
+        commentElement.innerHTML = `
+          <p><strong>Name:</strong> ${name}</p>
+          <p><strong>Rating:</strong> ${rating}</p>
+          <p><strong>Comment:</strong> ${comment}</p>
+        `;
+        commentsContainer.appendChild(commentElement);
+      });
     }
   }
 
-  const comment = commentInput.value;
-
-  const commentElement = document.createElement('div');
-  commentElement.classList.add('comment');
-  commentElement.innerHTML = `
-    <p><strong>Name:</strong> ${name}</p>
-    <p><strong>Rating:</strong> ${rating}</p>
-    <p><strong>Comment:</strong> ${comment}</p>
-  `;
-
-  commentsContainer.appendChild(commentElement);
-
-  saveComment({ name, rating, comment });
-
-  nameInput.value = '';
-  commentInput.value = '';
-  ratingInputs[0].checked = true;
-}
-
-function saveComment(comment) {
-  let comments = localStorage.getItem('comments');
-  if (!comments) {
-    comments = [comment];
-  } else {
-    comments = JSON.parse(comments);
-    comments.push(comment);
-  }
-
-  localStorage.setItem('comments', JSON.stringify(comments));
-}
-
-function loadComments() {
-  const comments = localStorage.getItem('comments');
-
-  if (comments) {
-    JSON.parse(comments).forEach(({ name, rating, comment }) => {
-      const commentElement = document.createElement('div');
-      commentElement.classList.add('comment');
-      commentElement.innerHTML = `
-        <p><strong>Name:</strong> ${name}</p>
-        <p><strong>Rating:</strong> ${rating}</p>
-        <p><strong>Comment:</strong> ${comment}</p>
-      `;
-      commentsContainer.appendChild(commentElement);
-    });
-  }
-}
-
-submitButton.addEventListener('click', submitComment);
-
-window.addEventListener('load', loadComments);
+  submitButton.addEventListener('click', submitComment);
+  window.addEventListener('load', loadComments);
